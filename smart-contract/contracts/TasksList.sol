9fa3c3a9641df69cb3d6630fc09fa8a18d21dcb3
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2021-02-28 02:20:07 +0400

    Toggle task archived status, get non archived tasks

--- smart-contract/contracts/TasksList.sol
+++ smart-contract/contracts/TasksList.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 contract TasksList {
     uint tasksCount = 0;
+    uint archivedCount = 0;
 
     struct Task {
       uint id;
       string text;
       bool isCompleted;
-      bool isRemoved;
+      bool isArchived;
     }
 
     mapping(uint => Task) public tasks;
@@ -17,12 +19,17 @@ contract TasksList {
       uint id,
       string text,
       bool isCompleted,
-      bool isRemoved
+      bool isArchived
     );
 
     event TaskCompleted(
       uint id,
       bool isCompleted
+    );
+
+    event TaskArchived(
+      uint id,
+      bool isArchived
     );
 
     constructor() public {
@@ -33,6 +40,20 @@ contract TasksList {
 
     function getTasksCount() public view returns (uint) {
       return tasksCount;
+    }
+
+    function getTasks() public view returns (Task[] memory) {
+      Task[] memory taskArr = new Task[](tasksCount - archivedCount);
+
+      for (uint i = 0; i < tasksCount; i++) {
+        Task memory _task = tasks[i + 1]; 
+        
+        if (!_task.isArchived) {
+          taskArr[i] = _task;
+        }
+      }
+
+      return taskArr;
     }
 
     function createTask(string memory _text) public {
@@ -48,5 +69,19 @@ contract TasksList {
       tasks[_id] = _task;
 
       emit TaskCompleted(_id, _task.isCompleted);
+    }
+
+    function toggleArchived(uint _id) public {
+      Task memory _task = tasks[_id];
+      _task.isArchived = !_task.isArchived;
+      tasks[_id] = _task;
+
+      if (_task.isArchived) {
+        archivedCount++;
+      } else {
+        archivedCount--;
+      }
+
+      emit TaskArchived(_id, _task.isArchived);
     }
 }
--- smart-contract/test/TasksList.test.js
+++ smart-contract/test/TasksList.test.js
@@ -17,7 +17,7 @@ contract("TasksList", (accounts) => {
   });
 
   it("Lists tasks", async () => {
-    const tasksCount = await this.tasksList.tasksCount();
+    const tasksCount = await this.tasksList.getTasksCount();
     const firstTask = await this.tasksList.tasks(1);
 
     assert.strictEqual(firstTask.id.toNumber(), 1);
@@ -26,17 +26,28 @@ contract("TasksList", (accounts) => {
     assert.strictEqual(tasksCount.toNumber(), 3);
   });
 
+  it("Gets tasks", async () => {
+    const tasksCount = await this.tasksList.getTasksCount();
+    const tasks = await this.tasksList.getTasks();
+    const firstTask = tasks[0];
+
+    assert.strictEqual(tasks.length, tasksCount.toNumber());
+    assert.strictEqual(firstTask.id, "1");
+    assert.strictEqual(firstTask.text, "Write your everyday tasks in here.");
+    assert.strictEqual(firstTask.isCompleted, false);
+  });
+
   it("Creates tasks", async() => {
     const taskText = "4th Task";
     const result = await this.tasksList.createTask(taskText);
-    const tasksCount = await this.tasksList.tasksCount();
+    const tasksCount = await this.tasksList.getTasksCount();
     const event = result.logs[0].args;
 
     assert.strictEqual(tasksCount.toNumber(), 4);
     assert.strictEqual(event.id.toNumber(), 4);
     assert.strictEqual(event.text, taskText);
     assert.strictEqual(event.isCompleted, false);
-    assert.strictEqual(event.isRemoved, false);
+    assert.strictEqual(event.isArchived, false);
   });
 
   it("Changes task completed status", async () => {
@@ -47,5 +58,15 @@ contract("TasksList", (accounts) => {
     assert.strictEqual(task.isCompleted, false);
     assert.strictEqual(event.id.toNumber(), 1);
     assert.strictEqual(event.isCompleted, true);
+  });
+
+  it("Changes task toggle status", async () => {
+    const task = await this.tasksList.tasks(1);
+    const result = await this.tasksList.toggleArchived(1);
+    const event = await result.logs[0].args;
+
+    assert.strictEqual(task.isArchived, false);
+    assert.strictEqual(event.id.toNumber(), 1);
+    assert.strictEqual(event.isArchived, true);
   });
 });
